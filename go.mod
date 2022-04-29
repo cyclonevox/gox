68e2243c7d2e00755be46063fab2f2fba0287366
yangteng@class100.com <Ybjy123>
2022-04-29 09:57:21 +0800

    添加无敌的多字段排序

--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.16
 require (
 	github.com/mcuadros/go-defaults v1.2.0
 	github.com/mitchellh/mapstructure v1.3.3
+	github.com/olivere/elastic/v7 v7.0.32
 	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e
-	golang.org/x/text v0.3.6
+	golang.org/x/text v0.3.7
 	xorm.io/xorm v1.3.0
 )
--- paging.go
+++ paging.go
@@ -3,7 +3,18 @@ package gox
 import (
 	`fmt`
 	`strings`
+
+	`github.com/olivere/elastic/v7`
 )
+
+type SortBy string
+
+type sorters []sorter
+
+type sorter struct {
+	Field     string
+	Ascending bool
+}
 
 // Paging 分页对象
 type Paging struct {
@@ -17,6 +28,67 @@ type Paging struct {
 	SortOrder string `default:"DESC" json:"sortOrder" param:"sortOrder" query:"sortOrder" form:"sortOrder" xml:"sortOrder"  validate:"oneof=asc ASC ascending ASCENDING desc DESC descending DESCENDING"`
 }
 
+func (s SortBy) OrderBy() string {
+	return s.sorters().orderBy()
+}
+
+func (s SortBy) Sorters() []elastic.Sorter {
+	return s.sorters().Sorters()
+}
+
+func (s SortBy) sorters() sorters {
+	fields := strings.Split(string(s), ",")
+	if len(fields) == 0 {
+		return nil
+	}
+
+	sorters := make(sorters, 0, len(fields))
+	for _, field := range fields {
+		sortBy := strings.Split(field, " ")
+		if len(sortBy) > 2 {
+			continue
+		}
+
+		sorters = append(sorters, sorter{
+			Field:     sortBy[0],
+			Ascending: len(sortBy) == 2 && strings.ToUpper(sortBy[1]) == "ASC",
+		})
+	}
+
+	return sorters
+}
+
+func (s sorters) orderBy() string {
+	if len(s) == 0 {
+		return ""
+	}
+
+	b := strings.Builder{}
+	for _, sorter := range s {
+		order := "DESC"
+		if sorter.Ascending {
+			order = "ASC"
+		}
+
+		b.WriteString(fmt.Sprintf("`%s` %s,", sorter.Field, order))
+	}
+
+	orderBy := b.String()
+
+	return orderBy[:len(orderBy)-1]
+}
+
+func (s sorters) Sorters() []elastic.Sorter {
+	sorters := make([]elastic.Sorter, 0, len(s))
+	for _, sorter := range s {
+		sorters = append(sorters, elastic.SortInfo{
+			Field:     sorter.Field,
+			Ascending: sorter.Ascending,
+		})
+	}
+
+	return sorters
+}
 
 // OrderBy 排序字符串
 func (p *Paging) OrderBy(sorter Sorter) string {

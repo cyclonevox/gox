a1798d10bc4a14a4cecf07a5967219cc0639d3dc
yangteng@class100.com <Ybjy123>
2022-05-11 10:44:51 +0800

    添加xorm的redis缓存，以及fill方法限制goroutine数量

--- concurrency/fill.go
+++ concurrency/fill.go
@@ -9,25 +9,47 @@ import (
 type FillFunc func(i int)
 
 // Fills 并发将slice切片（需要元素为相同类型）里的每一个元素，作为fun的参数并调用fun
-func Fills(slice interface{}, fun FillFunc) {
+func Fills(slice interface{}, fun FillFunc, maxGoroutines ...int) {
+	max := 20
+	if len(maxGoroutines) != 0 && maxGoroutines[0] > 0 {
+		max = maxGoroutines[0]
+	}
+
 	value := reflect.ValueOf(slice)
 	if value.Kind() != reflect.Slice {
 		return
 	}
 
-	var wg sync.WaitGroup
+	var (
+		offset       int
+		length       = value.Len()
+		groups       = length/max + 1
+		lastGroupNum = length % max
+	)
 
-	wg.Add(value.Len())
-	for i := 0; i < value.Len(); i++ {
-		go func(i int) {
-			if r := recover(); r != nil {
-				fmt.Println("Fills方法发生panic")
-			}
+	for i := 0; i < groups; i++ {
+		groupNum := max
+		if i == groups-1 {
+			groupNum = lastGroupNum
+		}
 
-			fun(i)
+		wg := sync.WaitGroup{}
+		wg.Add(groupNum)
+		for j := 0; j < groupNum; j++ {
+			go func(j int) {
+				defer func() {
+					if r := recover(); r != nil {
+						fmt.Println("调用fills方法发生panic")
+					}
 
-			wg.Done()
-		}(i)
+					wg.Done()
+				}()
+
+				fun(j + offset)
+			}(j)
+		}
+		wg.Wait()
+
+		offset += groupNum
 	}
-	wg.Wait()
 }
